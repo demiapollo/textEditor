fe24a9130795e8740f14b42b146edf7049221c54
demiapollo <apollodemirel67@gmail.com>
2023-06-22 12:39:31 -0700

    webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,9 +12,6 @@ module.exports = () => {
     entry: {
       main: "./src/js/index.js",
       install: "./src/js/install.js",
-      // database: "./src/js/database.js",
-      // header: "./src/js/header.js",
-      // editor: "./src/js/editor.js",
     },
     output: {
       filename: "[name].bundle.js",
